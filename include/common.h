cc8287ad14399320e48b30b6c53985386dd58b1a
e7 <ryuuzaki.uchiha@gmail.com>
2012-08-25 16:28:08 +0800

    增加查找接口。

--- include/common.h
+++ include/common.h
@@ -23,6 +23,8 @@
 
 
 // common headers
+#include <stddef.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
---
+++ include/search.h
@@ -0,0 +1,12 @@
+// search.h
+// author: e7, ryuuzaki.uchiha@gmail.com
+
+
+#ifndef __SEARCH_H__
+#define __SEARCH_H__
+
+
+#include "common.h"
+
+
+#endif // __SEARCH_H__
