eb68bfc2587e2e29bcbbf595cdc85c995b45c620
e7 <ryuuzaki.uchiha@gmail.com>
2012-10-24 00:53:16 +0800

    定义avl树结点结构。

--- include/avltree_frame.h
+++ include/avltree_frame.h
@@ -3,5 +3,17 @@
 
 
 #ifndef __AVLTREE_FRAME_H__
-#defein __AVLTREE_FRAME_H__
+#define __AVLTREE_FRAME_H__
+
+#include "common.h"
+
+
+typedef struct avltree_frame_node avltree_frame_node_t;
+
+struct avltree_frame_node {
+    int m_height; // 结点高度
+    avltree_frame_node_t *mp_father; // 父结点
+    avltree_frame_node_t *mp_lchild; // 左儿子
+    avltree_frame_node_t *mp_rchild; // 右儿子
+};
 #endif // __AVLTREE_FRAME_H__
