2c3712185196b2fceefa093afc1972d2e3e60f8f
ryuuzaki <jackzxty@126.com>
2012-10-25 10:15:17 +0800

    修改avl树结构为递归定义。

--- include/avltree_frame.h
+++ include/avltree_frame.h
@@ -1,4 +1,4 @@
-// avltree_frame.h: 2-3-4树的二叉树实现。
+// avltree_frame.h: avl树实现。
 // author: e7, ryuuzaki.uchiha@gmail.com
 
 
@@ -8,28 +8,42 @@
 #include "common.h"
 
 
-typedef struct avltree_frame_node avltree_frame_node_t;
-
-struct avltree_frame_node {
+typedef struct avltree_frame avltree_frame_t;
+struct avltree_frame {
     int m_key; // 键
-    int m_height; // 结点高度
-    avltree_frame_node_t *mp_father; // 父结点
-    avltree_frame_node_t *mp_lchild; // 左儿子
-    avltree_frame_node_t *mp_rchild; // 右儿子
+    int m_height; // 树高度
+    avltree_frame_t *mp_ftree; // 父树
+    avltree_frame_t *mp_ltree; // 左子树
+    avltree_frame_t *mp_rtree; // 右子树
+    int m_size; // 树结点数目
 };
 
 #define AVLTREE_FRAME_NODE(name)         \
             avltree_frame_node_t name = {0, 0, NULL, NULL, NULL}
 
+// ********** avl树接口 **********
 static inline
-void init_avltree_frame_node_orig(avltree_frame_node_t *p_node, int key)
+void init_avltree_frame(avltree_frame_t *p_tree, int key)
 {
-    ASSERT(NULL != p_node);
+    ASSERT(NULL != p_tree);
 
-    p_node->m_key = key;
-    p_node->m_height = 0;
-    p_node->mp_father = NULL;
-    p_node->mp_lchild = NULL;
-    p_node->mp_rchild = NULL;
+    p_tree->m_key = key;
+    p_tree->m_height = 0;
+    p_tree->mp_ftree = NULL;
+    p_tree->mp_ltree = NULL;
+    p_tree->mp_rtree = NULL;
 }
+
+static inline
+int insert_avltree_frame(avltree_frame_t *p_tree,
+                         avltree_frame_t *p_subtree)
+{
+    int rslt = 0;
+
+    ASSERT(NULL != p_tree);
+    ASSERT(NULL != p_subtree);
+
+    return rslt;
+}
+
 #endif // __AVLTREE_FRAME_H__
