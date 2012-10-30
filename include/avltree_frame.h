d97cdfce5d4b856f35f7fd26197a84f3147f5521
ryuuzaki <jackzxty@126.com>
2012-10-30 17:03:12 +0800

    实现avl树插入函数框架。

--- include/avltree_frame.h
+++ include/avltree_frame.h
@@ -21,6 +21,11 @@ struct avltree_frame {
 #define AVLTREE_FRAME_NODE(name)         \
             avltree_frame_node_t name = {0, 0, NULL, NULL, NULL}
 
+
+// ********** 二叉树的四种旋转 **********
+
+
+
 // ********** avl树接口 **********
 static inline
 void init_avltree_frame(avltree_frame_t *p_tree, int key)
@@ -39,29 +44,59 @@ int insert_avltree_frame(avltree_frame_t *p_tree,
                          avltree_frame_t *p_subtree)
 {
     int rslt = 0;
-    avltree_frame_t **pp_father = NULL;
+    int const UPWARD_COUNT = 3; // 最多影响曾祖父结点
+    avltree_frame_t *p_ftree = NULL;
+    avltree_frame_t **pp_vacancy = NULL;
+    avltree_frame_t *p_child_tree = NULL;
 
+    // 参数检查
     ASSERT(NULL != p_tree);
     ASSERT(NULL != p_subtree);
 
-    // 执行bst插入
-    pp_father = &p_tree;
-    while (NULL != *pp_father) {
-        if ((*pp_father)->m_key < p_subtree->m_key) {
-            pp_father = &(*pp_father)->mp_ltree;
-        } else if ((*pp_father)->m_key > p_subtree->m_key) {
-            pp_father = &(*pp_father)->mp_rtree;
+    // 只允许插入单结点子树
+    ASSERT(0 == p_subtree->m_balance_factor);
+    ASSERT((NULL == p_subtree->mp_ltree) && (NULL == p_subtree->mp_rtree));
+
+    // 寻找插入位置
+    pp_vacancy = &p_tree;
+    while (NULL != *pp_vacancy) {
+        p_ftree = (*pp_vacancy); // 更新父树
+
+        if ((*pp_vacancy)->m_key < p_subtree->m_key) {
+            pp_vacancy = &(*pp_vacancy)->mp_ltree;
+        } else if ((*pp_vacancy)->m_key > p_subtree->m_key) {
+            pp_vacancy = &(*pp_vacancy)->mp_rtree;
         } else {
             break;
         }
     }
-    if (NULL != *pp_father) {
+    if (NULL != *pp_vacancy) {
         rslt = -1;
         goto FINAL;
     }
-    (*pp_father) = p_subtree;
+
+    // 执行插入
+    p_subtree->mp_ftree = p_ftree;
+    (*pp_vacancy) = p_subtree;
 
     // 调整
+    p_child_tree = p_subtree;
+    for (int i = 0; i < UPWARD_COUNT; ++i) {
+        if (p_child_tree == p_ftree->mp_ltree) {
+            --(p_ftree->m_balance_factor);
+        } else if (p_child_tree == p_ftree->mp_rtree) {
+            ++(p_ftree->m_balance_factor);
+        } else {
+            ASSERT(0);
+        }
+
+        if (p_ftree->m_balance_factor < -1) {
+        } else if (p_ftree->m_balance_factor > 1) {
+        } else {
+            p_child_tree = p_ftree;
+            p_ftree = p_ftree->mp_ftree;
+        }
+    }
 
 FINAL:
     return rslt;
--- include/error_info.h
+++ include/error_info.h
@@ -6,4 +6,10 @@
 #define __ERROR_INFO_H__
 
 #include "common.h"
+
+
+typedef enum {
+    E_OK = 0,
+    E_EXISTED,
+} errno_t;
 #endif // __ERROR_INFO_H__
