22f496e95f18a46d79a5aa2dd35082352671d44e
ryuuzaki <jackzxty@126.com>
2013-04-15 14:25:51 +0800

    增加hash_base.h文件，基础散列实现

---
+++ include/hash_base.h
@@ -0,0 +1,20 @@
+// hash_base.h
+// author: e7, ryuuzaki.uchiha@gmail.com
+
+
+#ifndef __HASH_BASE_H__
+#define __HASH_BASE_H__
+
+
+#include "common.h"
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif // __HASH_BASE_H__
